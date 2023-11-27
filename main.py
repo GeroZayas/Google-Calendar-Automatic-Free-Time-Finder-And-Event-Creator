83476daa8dffa2a3e04c4c7e670b4ebca216f617
Gero Zayas <gerozayas@gmail.com>
2023-11-27 21:10:34 +0100

    Add find free time option

--- main.py
+++ main.py
@@ -1,40 +1,85 @@
 import PySimpleGUI as sg
+import datetime
+import pytz
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-import datetime
 
 
 # Function to authenticate and create a service object
 def authenticate_google():
     flow = InstalledAppFlow.from_client_secrets_file(
-        "client_secret_591796920211-6g24f2ol62malqlvsmlvjncb3ah8opps.apps.googleusercontent.com.json",
-        scopes=["https://www.googleapis.com/auth/calendar"],
+        "credentials.json", scopes=["https://www.googleapis.com/auth/calendar"]
     )
     creds = flow.run_local_server(port=0)
-    service = build("calendar", "v3", credentials=creds)
-    return service
+    return build("calendar", "v3", credentials=creds)
+
+
+# Function to find free time slots
+def find_free_time(service, date):
+    timezone = pytz.timezone("Europe/Madrid")
+    start_date = datetime.datetime.strptime(date, "%Y-%m-%d").replace(tzinfo=timezone)
+    end_date = start_date + datetime.timedelta(days=1)
+
+    # Formatting start_date and end_date for the API request
+    formatted_start_date = start_date.isoformat()
+    formatted_end_date = end_date.isoformat()
+
+    body = {
+        "timeMin": formatted_start_date,
+        "timeMax": formatted_end_date,
+        "items": [{"id": "primary"}],
+    }
+
+    events_result = service.freebusy().query(body=body).execute()
+    busy_times = events_result["calendars"]["primary"]["busy"]
+
+    free_slots = []
+    current_time = start_date
+
+    for busy_period in busy_times:
+        busy_start = datetime.datetime.fromisoformat(busy_period["start"]).astimezone(
+            timezone
+        )
+        while current_time < busy_start:
+            free_slots.append(current_time.isoformat())
+            current_time += datetime.timedelta(hours=1)
+        busy_end = datetime.datetime.fromisoformat(busy_period["end"]).astimezone(
+            timezone
+        )
+        current_time = busy_end
+
+    # Check for free time after the last busy period
+    while current_time < end_date:
+        free_slots.append(current_time.isoformat())
+        current_time += datetime.timedelta(hours=1)
+
+    return free_slots
 
 
 # Function to create an event in Google Calendar
-def create_event(service, title, start_date, end_date, description):
+def create_event(service, title, start_datetime, end_datetime, description):
     event = {
         "summary": title,
         "description": description,
-        "start": {"date": start_date, "timeZone": "Europe/Madrid"},
-        "end": {"date": end_date, "timeZone": "Europe/Madrid"},
+        "start": {"dateTime": start_datetime, "timeZone": "Europe/Madrid"},
+        "end": {"dateTime": end_datetime, "timeZone": "Europe/Madrid"},
     }
     event = service.events().insert(calendarId="primary", body=event).execute()
-    print("Event created: %s" % (event.get("htmlLink")))
+    return event.get("htmlLink")
 
 
 # Define the window's contents for PySimpleGUI
 layout = [
     [sg.Text("Create an Event in Google Calendar")],
     [sg.Text("Event Title"), sg.InputText(key="title")],
-    [sg.Text("Start Date (YYYY-MM-DD)"), sg.InputText(key="start_date")],
-    [sg.Text("End Date (YYYY-MM-DD)"), sg.InputText(key="end_date")],
+    [
+        sg.Text("Event Date"),
+        sg.Input(key="event_date"),
+        sg.CalendarButton("Choose Date", target="event_date", format="%Y-%m-%d"),
+    ],
     [sg.Text("Description"), sg.InputText(key="description")],
-    [sg.Button("Submit"), sg.Button("Cancel")],
+    [sg.Button("Find Free Time"), sg.Button("Submit"), sg.Button("Cancel")],
+    [sg.Text("", size=(40, 1), key="free_time_info")],
 ]
 
 # Create the window
@@ -43,16 +88,28 @@ window = sg.Window("Event Creator", layout)
 # Event loop
 while True:
     event, values = window.read()
+
     if event in (None, "Cancel"):
         break
     elif event == "Submit":
+        # Here, you should include logic to select a specific free time slot
+        # For now, it will require manual entry or modification
         service = authenticate_google()
-        create_event(
+        event_link = create_event(
             service,
             values["title"],
-            values["start_date"],
-            values["end_date"],
+            values["event_date"] + "T09:00:00",
+            values["event_date"] + "T10:00:00",
             values["description"],
         )
+        sg.popup(f"Event created: {event_link}")
+    elif event == "Find Free Time":
+        if values["event_date"]:
+            service = authenticate_google()
+            free_time_slots = find_free_time(service, values["event_date"])
+            # You can update this part to let the user select from free_time_slots
+            window["free_time_info"].update(f"Free slots: {free_time_slots}")
+        else:
+            sg.popup("Please select a date first")
 
 window.close()
