9dbb6c2c80bf68b43e168e17393a7a84e6b1b019
Gero Zayas <gerozayas@gmail.com>
2023-12-09 07:50:13 +0100

    Fix credentials

--- google_calendar_event_creator.py
+++ google_calendar_event_creator.py
@@ -11,23 +11,24 @@ from google.auth.transport.requests import Request
 # Function to authenticate and create a service object
 def authenticate_google():
     creds = None
-    # The file token.json stores the user's access and refresh tokens and is
-    # created automatically when the authorization flow completes for the first time.
     if os.path.exists("token.json"):
         creds = google.oauth2.credentials.Credentials.from_authorized_user_file(
             "token.json", scopes=["https://www.googleapis.com/auth/calendar"]
         )
 
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
+            # Delete the token.json file if it exists
+            if os.path.exists("token.json"):
+                os.remove("token.json")
+
             flow = InstalledAppFlow.from_client_secrets_file(
                 "credentials.json", scopes=["https://www.googleapis.com/auth/calendar"]
             )
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
+
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "/home/gero/Downloads/💻💻 CODING 💻💻/Google-Calendar-PySimpleGui"
+source env/bin/activate
+python3 google_calendar_event_creator.py
